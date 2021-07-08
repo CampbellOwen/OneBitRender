01438091160e1421788b294a51f7478c80b1d493
Owen Campbell <ocampbell95@gmail.com>
2021-07-07 20:28:34 -0700

    Hopefully optimize CopyImageToTexture a little

--- apps/main.cpp
+++ apps/main.cpp
@@ -14,18 +14,14 @@ void CopyImageToTexture(SDL_Texture* texture, const uint8_t* pixels)
     int32_t texPitch = 0;
     SDL_LockTexture(texture, NULL, reinterpret_cast<void**>(&texPixels), &texPitch);
 
-    for (int y = 0; y < OneBit::RENDER_HEIGHT; y++)
+    for (int i = 0; i < OneBit::RENDER_WIDTH * OneBit::RENDER_HEIGHT; i++)
     {
-        for (int x = 0; x < OneBit::RENDER_WIDTH; x++)
-        {
-            uint32_t index = y * OneBit::RENDER_WIDTH + x;
-
-            uint8_t colour = pixels[index];
-            texPixels[(index * 4) + 0] = 255;
-            texPixels[(index * 4) + 1] = pixels[index];
-            texPixels[(index * 4) + 2] = pixels[index];
-            texPixels[(index * 4) + 3] = pixels[index];
-        }
+        uint32_t index = i * 4;
+        uint8_t colour = pixels[i];
+        texPixels[index] = 255;
+        texPixels[index + 1] = colour;
+        texPixels[index + 2] = colour;
+        texPixels[index + 3] = colour;
     }
 
     SDL_UnlockTexture(texture);
@@ -77,6 +73,8 @@ int main()
 
             uint32_t t = 0;
 
+            auto shadingType = OneBit::ShadingType::FLAT;
+
             // Event loop
             while (!quit)
             {
@@ -92,13 +90,19 @@ int main()
                     }
                 }
 
+                // if (t % 1000 == 0)
+                //{
+                //    shadingType = shadingType == OneBit::ShadingType::FLAT ? OneBit::ShadingType::WIREFRAME
+                //                                                           : OneBit::ShadingType::FLAT;
+                //}
+
                 vertices[0].y = 0.25 + (0.2 * sin(0.01 * t));
                 vertices[1].y = 0.75 + (0.2 * sin(0.01 * t + 10));
                 vertices[2].y = 0.75 + (0.2 * sin(0.01 * t + 20));
 
                 obRenderer.Clear(255);
 
-                obRenderer.SetShadingType(OneBit::ShadingType::FLAT);
+                obRenderer.SetShadingType(shadingType);
                 obRenderer.Render(vertices);
 
                 obRenderer.DrawLine({{50, 239}, {350, 120}});
